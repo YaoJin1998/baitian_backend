1d42d38a0ebca52dcb7e051ce90b5e518f69e4c6
yaojin1234 <12538264492@qq.com>
2021-03-21 16:45:52 +0800

    批量删除数据的代码实现

--- biantian_service/sevice_oss/src/main/java/com/xmut/oss/controller/FileController.java
+++ biantian_service/sevice_oss/src/main/java/com/xmut/oss/controller/FileController.java
@@ -4,15 +4,13 @@ package com.xmut.oss.controller;
 import com.xmut.commonutils.R;
 import com.xmut.oss.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/eduoss/fileoss")
 public class FileController {
